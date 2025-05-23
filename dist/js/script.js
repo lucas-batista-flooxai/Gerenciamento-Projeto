334446fc2d1c76b697c49474a9651e66d7190d7e
Lucas <lucas.batista.flooxai@gmail.com>
2025-05-23 12:42:02 -0300

    feat: Json fields

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,25 +1,28 @@
-var name = document.querySelector("#exampleInputName");
-var gender = document.querySelectorAll(
-  "#form-user-create [name-gender]:checked"
-);
-var birth = document.querySelector("exampleInputBirth");
-var coutry = document.querySelector("exampleInputCountry");
-var email = document.querySelector("exampleInputEmail");
-var password = document.querySelector("exampleInputPassword");
-var photo = document.querySelector("exampleInputFile");
-var admin = document.querySelector("#exampleInputAdmin");
+// var name = document.querySelector("#exampleInputName");
+// var gender = document.querySelectorAll(
+//   "#form-user-create [name-gender]:checked"
+// );
+// var birth = document.querySelector("exampleInputBirth");
+// var coutry = document.querySelector("exampleInputCountry");
+// var email = document.querySelector("exampleInputEmail");
+// var password = document.querySelector("exampleInputPassword");
+// var photo = document.querySelector("exampleInputFile");
+// var admin = document.querySelector("#exampleInputAdmin");
 
 var field = document.querySelectorAll("#form-user-create [name]");
-field.forEach(function(field, index) {
 
-    if (field.name == "gender") {
-        if(field.checked) {
-            console.log("SIM", field)
-        } 
-    } else {
-        console.log("Não")
+var user = {};
+
+field.forEach(function (field, index) {
+  if (field.name == "gender") {
+    if (field.checked) {
+      user[field.name] = field.value
     }
+  } else {
+    user[field.name] = field.value
+  }
 
-    // console.log(field.id, field.name, field.value, field.checked, index)
-
+  // console.log(field.id, field.name, field.value, field.checked, index)
 });
+
+console.log(user)
