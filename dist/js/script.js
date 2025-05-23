6f2dfc306787304475a17eff11a32fdbb49061a3
Lucas <lucas.batista.flooxai@gmail.com>
2025-05-23 13:30:40 -0300

    feat: functions and createElement

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,17 +1,41 @@
-// var name = document.querySelector("#exampleInputName");
-// var gender = document.querySelectorAll(
-//   "#form-user-create [name-gender]:checked"
-// );
-// var birth = document.querySelector("exampleInputBirth");
-// var coutry = document.querySelector("exampleInputCountry");
-// var email = document.querySelector("exampleInputEmail");
-// var password = document.querySelector("exampleInputPassword");
-// var photo = document.querySelector("exampleInputFile");
-// var admin = document.querySelector("#exampleInputAdmin");
-
 var field = document.querySelectorAll("#form-user-create [name]");
 
 var user = {};
+
+function addLine(dataUser) {
+  var tr = document.createElement("tr");
+  tr.innerHTML = `
+    <tr>
+        <td>
+            <img
+                src="dist/img/user1-128x128.jpg"
+                    alt="User Image"
+                    class="img-circle img-sm"
+                    />
+                    
+                        <td>${dataUser.name}</td>
+                        <td>${dataUser.name}</td>
+                        <td>${dataUser.admin}</td>
+                        <td>${dataUser.birth}</td>
+                        <td>
+                          <button
+                            type="button"
+                            class="btn btn-primary btn-xs btn-flat"
+                        >
+                        Editar
+                        </button>
+                        <button
+                        type="button"
+                        class="btn btn-danger btn-xs btn-flat"
+                        >
+                    Excluir
+                </button>
+            </td>
+        </tr>
+    `;
+
+    document.getElementById('table-users').appendChild(tr);
+}
 
 document
   .getElementById("form-user-create")
@@ -28,5 +52,5 @@ document
       }
     });
 
-    console.log(user);
+    addLine(user);
   });
