8891203c7a810f2403dcce07e487726b98efcdcc
Lucas <lucas.batista.flooxai@gmail.com>
2025-05-23 12:04:33 -0300

    feat: add forEach in form fields

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,8 +1,17 @@
-var name = document.querySelector("#exampleInputName")
-var gender = document.querySelectorAll("#form-user-create [name-gender]:checked")
-var birth = document.querySelector("exampleInputBirth")
-var coutry = document.querySelector("exampleInputCountry")
-var email = document.querySelector("exampleInputEmail")
-var password = document.querySelector("exampleInputPassword")
-var photo = document.querySelector("exampleInputFile")
-var admin = document.querySelector("#")
+var name = document.querySelector("#exampleInputName");
+var gender = document.querySelectorAll(
+  "#form-user-create [name-gender]:checked"
+);
+var birth = document.querySelector("exampleInputBirth");
+var coutry = document.querySelector("exampleInputCountry");
+var email = document.querySelector("exampleInputEmail");
+var password = document.querySelector("exampleInputPassword");
+var photo = document.querySelector("exampleInputFile");
+var admin = document.querySelector("#exampleInputAdmin");
+
+var field = document.querySelectorAll("#form-user-create [name]");
+field.forEach(function(field, index) {
+
+    console.log(field.id, field.name, field.value, field.checked, index)
+
+});
