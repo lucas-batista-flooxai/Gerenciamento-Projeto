b85fc9e8a3c1ebcd094f834c8ba5ce9541e4f7f5
Lucas <lucas.batista.flooxai@gmail.com>
2025-05-26 12:55:21 -0300

    feat: add Promise

--- controller/UserController.js
+++ controller/UserController.js
@@ -10,7 +10,42 @@ class UserController {
     this.formEl.addEventListener("submit", (event) => {
       event.preventDefault();
 
-      this.addLine(this.getValues());
+      let values = this.getValues();
+
+      this.getPhoto().then(
+        (content) => {
+          values.photo = content;
+
+          this.addLine(values);
+        },
+        (e) => {
+          console.error(e);
+        }
+      );
+    });
+  }
+
+  getPhoto() {
+    return new Promise((resolve, reject) => {
+      let fileReader = new FileReader();
+
+      let elements = [...this.formEl.elements].filter((item) => {
+        if (item.name === "photo") {
+          return item;
+        }
+      });
+
+      let file = elements[0].files[0];
+
+      fileReader.onload = () => {
+        resolve(fileReader.result);
+      };
+
+      fileReader.onerror = (e) => {
+        reject(e);
+      };
+
+      fileReader.readAsDataURL(file);
     });
   }
 
@@ -42,7 +77,7 @@ class UserController {
   addLine(dataUser) {
     this.tableEl.innerHTML = `
             <tr>
-                <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+                <td><img src=${dataUser.photo} class="img-circle img-sm"></td>
                 <td>${dataUser.name}</td>
                 <td>${dataUser.email}</td>
                 <td>${dataUser.admin}</td>
