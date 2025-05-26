64343aaf723e34f55501342cf28599c4977a013a
Lucas <lucas.batista.flooxai@gmail.com>
2025-05-26 13:57:52 -0300

    feat: add data, getters and setters

--- controller/UserController.js
+++ controller/UserController.js
@@ -10,6 +10,10 @@ class UserController {
     this.formEl.addEventListener("submit", (event) => {
       event.preventDefault();
 
+      let btn = this.formEl.querySelector("[type=submit]")
+
+      btn.disabled = true;
+
       let values = this.getValues();
 
       this.getPhoto().then(
@@ -17,6 +21,10 @@ class UserController {
           values.photo = content;
 
           this.addLine(values);
+
+          this.formEl.reset();
+
+          btn.disabled = false;
         },
         (e) => {
           console.error(e);
@@ -88,7 +96,7 @@ class UserController {
                 <td>${dataUser.name}</td>
                 <td>${dataUser.email}</td>
                 <td>${dataUser.admin ? "Sim" : "Não"}</td>
-                <td>${dataUser.birth}</td>
+                <td>${dataUser.register}</td>
                 <td>
                     <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                     <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
