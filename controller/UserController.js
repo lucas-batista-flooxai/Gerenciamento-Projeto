6358b4db56fddb843d88b81182ecf92bce9f7cfd
Lucas <lucas.batista.flooxai@gmail.com>
2025-05-27 11:57:41 -0300

    feat: edit user

--- controller/UserController.js
+++ controller/UserController.js
@@ -4,6 +4,7 @@ class UserController {
     this.tableEl = document.getElementById(tableId);
 
     this.onSubmit();
+    this.onEditCancel();
   }
 
   onSubmit() {
@@ -34,6 +35,14 @@ class UserController {
       );
     });
   }
+
+  onEditCancel = () => {
+    document
+      .querySelector("#box-user-update .btn-cancel")
+      .addEventListener("click", (e) => {});
+
+      this.showPanelCreate();
+  };
 
   getPhoto() {
     return new Promise((resolve, reject) => {
@@ -115,15 +124,31 @@ class UserController {
                 <td>${dataUser.admin ? "Sim" : "Não"}</td>
                 <td>${Utils.dateFormat(dataUser.register)}</td>
                 <td>
-                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                    <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
                     <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
                 </td>
         `;
+
+    tr.querySelector(".btn-edit").addEventListener("click", (e) => {
+      console.log(JSON.parse(tr.dataset.user));
+      
+      this.showPanelUpdate()
+    });
 
     this.tableEl.appendChild(tr);
 
     this.updateCount();
   }
+
+  showPanelCreate = () => {
+    document.querySelector("#box-user-create").style.display = "block";
+    document.querySelector("#box-user-update").style.display = "none";
+  };
+
+  showPanelUpdate = () => {
+    document.querySelector("#box-user-create").style.display = "none";
+    document.querySelector("#box-user-update").style.display = "block";
+  };
 
   updateCount() {
     let numberUsers = 0;
