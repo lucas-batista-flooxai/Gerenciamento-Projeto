48175a4d59480284170780154724fdae233aa2f0
Lucas <lucas.batista.flooxai@gmail.com>
2025-05-27 14:42:30 -0300

    feat:add localStorage

--- controller/UserController.js
+++ controller/UserController.js
@@ -176,8 +176,8 @@ class UserController {
   getusersStorage() {
     let users = [];
 
-    if (sessionStorage.getItem("users")) {
-      users = JSON.parse(sessionStorage.getItem("users"));
+    if (localStorage.getItem("users")) {
+      users = JSON.parse(localStorage.getItem("users"));
     }
 
     return users;
@@ -200,7 +200,7 @@ class UserController {
 
     users.push(data);
 
-    sessionStorage.setItem("users", JSON.stringify(users));
+    localStorage.setItem("users", JSON.stringify(users));
   }
 
   addLine(dataUser) {
